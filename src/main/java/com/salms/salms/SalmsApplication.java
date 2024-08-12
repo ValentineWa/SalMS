7f0c8f8ffaccc932153e52d31dccb06c63275142
Valentine  Waweru <valentine.waweru@Valentines-MacBook-Pro.local>
2024-08-12 13:17:38 +0300

    Initialize Spring

---
+++ src/main/java/com/salms/salms/SalmsApplication.java
@@ -0,0 +1,13 @@
+package com.salms.salms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalmsApplication.class, args);
+	}
+
+}
